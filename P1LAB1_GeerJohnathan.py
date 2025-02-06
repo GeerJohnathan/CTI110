19a4139f9645eb305be774366c84c1d8e31569ec
GeerJohnathan <jag69728@gmail.com>
2025-02-06 10:16:14 -0500

    Add files via upload

---
+++ P1LAB1_GeerJohnathan.py
@@ -0,0 +1,10 @@
+#Johnathan Geer
+#04Feb2025
+#P1LAB1
+#Getting input from a user to display output
+
+print('Enter your first name:', end='')
+first_name = input()
+print('Enter your last name:', end='')
+last_name = input()
+print('Hello,', first_name, last_name+'!', "Welcome to CTI-110" )
